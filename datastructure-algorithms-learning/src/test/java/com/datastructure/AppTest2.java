813f2c0e23daa3b7de04bb771539974365c3f18f
Sarath <sarath.learning@gmail.com>
2023-01-17 11:33:19 +0530

    added ignore profile

---
+++ datastructure-algorithms-learning/src/test/java/com/datastructure/AppTest2.java
@@ -0,0 +1,20 @@
+package com.datastructure;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest2
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
